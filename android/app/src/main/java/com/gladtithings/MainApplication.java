23508bcf3fc85a16cf43625895dc9972dac2840e
GarridoL <lalaine.garrido@student.passerellesnumeriques.org>
2021-12-15 16:43:11 +0800

    updates

--- android/app/src/main/java/com/gladtithings/MainApplication.java
+++ android/app/src/main/java/com/gladtithings/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import co.il.nester.android.react.streetview.NSTStreetViewPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
